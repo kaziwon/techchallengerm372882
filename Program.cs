87a0fbaba08d4ebea61fc9cb2030bf000cadcba5
Joao Victor <joaocabral@192.168.0.8>
2026-07-11 09:15:13 -0300

    teste ci/cd

--- Program.cs
+++ Program.cs
@@ -132,6 +132,9 @@ app.UseHttpsRedirection();
 app.UseAuthentication();
 app.UseAuthorization();
 
+app.MapGet("/health", () => Results.Ok(new { status = "Healthy" }))
+    .AllowAnonymous();
+
 app.MapControllers();
 
 app.Run();
